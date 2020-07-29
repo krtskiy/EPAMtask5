806cc1a47a9cdc79c214efce90b394cfdf8c4146
jMushmyH <jMushmyH@email.com>
2020-07-29 23:01:57 +0300

    Update Part1Test.java

--- src/test/java/com/epam/rd/java/basic/practice5/Part1Test.java
+++ src/test/java/com/epam/rd/java/basic/practice5/Part1Test.java
@@ -5,11 +5,6 @@ import org.junit.Test;
 
 public class Part1Test {
 
-    @Test(timeout = 3000)
-    public void shouldPrintEveryThreadNameNoLongerThanThreeSeconds() {
-        Part1.printThreadNameEvery300ms();
-    }
-
     @Test
     public void should() {
         long before = System.currentTimeMillis();
