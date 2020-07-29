a4b04627c4792e7f8c0a6f20d34f56b9dedefe28
jMushmyH <jMushmyH@email.com>
2020-07-29 14:11:23 +0300

    Part1 test1

--- src/main/java/com/epam/rd/java/basic/practice5/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part1.java
@@ -2,8 +2,40 @@ package com.epam.rd.java.basic.practice5;
 
 public class Part1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread firstThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                printThreadNameEvery300ms();
+            }
+        });
 
+        SecondThread secondThread = new SecondThread();
+        firstThread.start();
+        firstThread.join();
+        secondThread.start();
     }
 
+    static void printThreadNameEvery300ms() {
+        long before = System.currentTimeMillis();
+        while (true) {
+            long after = System.currentTimeMillis();
+            if (after - before >= 2000) break;
+            try {
+                Thread.sleep(300);
+                System.out.println(Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+}
+
+class SecondThread extends Thread {
+    @Override
+    public void run() {
+        Part1.printThreadNameEvery300ms();
+    }
 }
