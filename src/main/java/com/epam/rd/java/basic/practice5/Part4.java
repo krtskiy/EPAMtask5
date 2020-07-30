1646f7fdb9fb0b898edfe19eadb36d0d97c35754
jMushmyH <jMushmyH@email.com>
2020-07-31 02:45:45 +0300

    Part4

--- src/main/java/com/epam/rd/java/basic/practice5/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part4.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 
 public class Part4 {
 
-    private static int[] inputArrInt = new int[400];
+    private static int[][] inputArrInt = new int[4][100];
     private static Logger logger = Logger.getLogger(Part4.class.getName());
     private static final String INTERRUPTED_MSG = "Thread is interrupted";
 
@@ -18,17 +18,18 @@ public class Part4 {
         Part4HelperClass.write4by100MatrixToFile();
 
         long beforeMultiThread = System.currentTimeMillis();
-        System.out.println(123);     //here should be parallelize search results
+        System.out.println(4);     //here should be parallelize search results
         long afterMultiThread = System.currentTimeMillis();
         System.out.println(afterMultiThread - beforeMultiThread);
 
         long beforeSingleThread = System.currentTimeMillis();
-        System.out.println(Part4HelperClass.findMaxValue());
+        System.out.println(Part4HelperClass.findMax());
         long afterSingleThread = System.currentTimeMillis();
         System.out.println(afterSingleThread - beforeSingleThread);
+
     }
 
-    private static class Part4HelperClass {
+    private static class Part4HelperClass implements Runnable{
 
         static void write4by100MatrixToFile() {
             StringBuilder numbers = new StringBuilder();
@@ -54,26 +55,56 @@ public class Part4 {
             }
         }
 
-        private static int findMaxValue() {
+        private static void splitMatrixIntoArray() {
             String input = Demo.readFile("part4.txt");
-            String[] inputArrStr = input.split(" \\s*");
-            for (int i = 0; i < inputArrStr.length; i++) {
-                inputArrInt[i] = Integer.parseInt(inputArrStr[i]);
+            String[] inputArrStr = input.split(System.lineSeparator());
+            String[][] inputArrStr2 = new String[inputArrStr.length][100];
+            for (int i = 0; i < inputArrStr2.length; i++) {
+                inputArrStr2[i] = inputArrStr[i].split(" ");
             }
-            int maxValue = inputArrInt[0];
-            for (int i = 1; i < inputArrInt.length; i++) {
+            for (int i = 0; i < inputArrInt.length; i++) {
+                for (int j = 0; j < inputArrInt[i].length; j++) {
+                    inputArrInt[i][j] = Integer.parseInt(inputArrStr2[i][j]);
+                }
+            }
+        }
+
+        private static int findMaxInArray(int[] arr) {
+            splitMatrixIntoArray();
+            int maxValue = arr[0];
+            for (int i = 0; i < arr.length; i++) {
                 try {
                     Thread.sleep(1);
                 } catch (InterruptedException e) {
                     logger.severe(INTERRUPTED_MSG);
                     Thread.currentThread().interrupt();
                 }
-                if (inputArrInt[i] > maxValue) {
-                    maxValue = inputArrInt[i];
+                if (arr[i] > maxValue) {
+                    maxValue = arr[i];
                 }
             }
             return maxValue;
         }
 
+        private static int findMax() {
+            int maxValue = findMaxInArray(inputArrInt[0]);
+            for (int i = 0; i < inputArrInt.length; i++) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    logger.severe(INTERRUPTED_MSG);
+                    Thread.currentThread().interrupt();
+                }
+                if (findMaxInArray(inputArrInt[i]) > maxValue) {
+                    maxValue = findMaxInArray(inputArrInt[i]);
+                }
+            }
+            return maxValue;
+        }
+
+        @Override
+        public void run() {
+            findMax();
+        }
     }
 }
