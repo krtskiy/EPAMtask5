a374aea2f544b5733a27561dcf8cdbe1d590cb52
jMushmyH <jMushmyH@email.com>
2020-08-03 16:21:50 +0300

    Part2

--- src/main/java/com/epam/rd/java/basic/practice5/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part2.java
@@ -1,9 +1,26 @@
 package com.epam.rd.java.basic.practice5;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
 public class Part2 {
 
-    public static void main(final String[] args) {
+    private static final InputStream SYS_DEFAULT_IN = System.in;
+    private static Logger logger = Logger.getLogger(Part3.class.getName());
+    private static final String INTERRUPTED_MSG = "Thread is interrupted";
 
+    public static void main(final String[] args) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Spam.main(null);
+            }
+        });
+        t.start();
+        System.setIn(new ByteArrayInputStream(System.lineSeparator().getBytes()));
+
+        System.setIn(SYS_DEFAULT_IN);
     }
 
 }
--- src/main/java/com/epam/rd/java/basic/practice5/Spam.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Spam.java
@@ -1,26 +1,71 @@
 package com.epam.rd.java.basic.practice5;
 
+import java.util.Scanner;
+
 public class Spam {
 
     private Thread[] threads;
+    private String[] messages;
+    private int[] delays;
 
     public Spam(final String[] messages, final int[] delays) {
+        this.threads = new Thread[messages.length];
+        this.messages = messages;
+        this.delays = delays;
 
     }
 
-    public static void main(final String[] args) {
-
+    public static void main(String[] args) {
+        Spam spam = new Spam(new String[]{"@@@", "bbbbbbb"}, new int[]{332, 999});
+        spam.start();
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextLine()) {
+            spam.stop();
+        }
     }
 
     public void start() {
-
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Worker(messages[i], delays[i]);
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
     }
 
     public void stop() {
-
+        for (Thread t : threads) {
+            t.interrupt();
+        }
+        Worker.running = false;
     }
 
     private static class Worker extends Thread {
+
+        private String message;
+        private int delay;
+        private static volatile boolean running = true;
+
+        Worker(String message, int delay) {
+            this.message = message;
+            this.delay = delay;
+        }
+
+        public Worker() {
+        }
+
+        @Override
+        public void run() {
+            while (running) {
+                try {
+                    Thread.sleep(delay);
+                    System.out.println(message);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
 
     }
 
